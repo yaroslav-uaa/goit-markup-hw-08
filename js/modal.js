612a92f35c5e52c549cfee3f157b6e042ff8284d
Yaroslav <ypakhachuk@gmail.com>
2020-09-12 20:52:59 +0300

    fix flex

--- js/modal.js
+++ js/modal.js
@@ -9,7 +9,6 @@
     refs.closeModalBtn.addEventListener('click', toggleModal);
   
     function toggleModal() {
-      document.body.classList.toggle("modal-open");
       refs.modal.classList.toggle('is-hidden');
     }
   })();
